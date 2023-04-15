4021c333fafca4c915a67bd83f285465bfa3b249
Peter Wicks <patricker@gmail.com>
2023-04-15 13:45:26 -0600

    0.0.5

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mhn",
-    version="0.0.4",
+    version="0.0.5",
     description="Minimalist Hierarchical Notation (MHN) library",
     author="Peter Wicks",
     author_email="peter@wicks.ninja",
